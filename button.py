73129065fabe1ae2b1b41b78a92a68db0bac6c01
Irina <ms.irina-98@mail.ru>
2024-03-26 23:30:06 +0300

    Added button Play

--- button.py
+++ button.py
@@ -18,6 +18,16 @@ class Button():
         self.rect.center = self.screen_rect.center
 
         # Сообщение кнопки создается только один раз
-        self.prep_msg(msg)
+        self._prep_msg(msg)
 
-    
+    def _prep_msg(self, msg):
+        """Преобразует msg в прямоугольник и выравнивает текст по центру"""
+        self.msg_image = self.font.render(msg, True, self.text_color, 
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Отображение пустой кнопки и вывод сообщения
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
