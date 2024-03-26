a7da7d8d71506eee74803fc23d1f51243b7779c4
Irina <ms.irina-98@mail.ru>
2024-03-26 18:36:06 +0300

    Added alien fleet

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ class Settings():
         self.ship_speed = 1.5
 
         # Параметры снаряда
-        self.bullet_speed = 1
+        self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = 8
         self.bullet_color = (255, 0, 229)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
