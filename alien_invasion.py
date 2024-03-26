0fd1e17e51426b9f51417387b80d2c6ba7b54be2
Irina <ms.irina-98@mail.ru>
2024-03-26 15:32:32 +0300

    Added limiting the number of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,8 +31,19 @@ class AlienInvasion():
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+            
+    def _update_bullets(self):
+            """Обновляет позиции снарядов и уничтожает старые снаряды"""
+            # Обновление позиции снарядов
+            self.bullets.update()
+
+            # Удаление снарядов, вышедших за край экрана
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            # print(len(self.bullets))
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши"""
@@ -64,8 +75,9 @@ class AlienInvasion():
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
--- settings.py
+++ settings.py
@@ -16,3 +16,4 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 8
         self.bullet_color = (255, 0, 229)
+        self.bullets_allowed = 3
