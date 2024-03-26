2a241a7629b59f08330cc7e8841681038f7b5759
Irina <ms.irina-98@mail.ru>
2024-03-26 23:48:58 +0300

    Added game difficulty increase

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,7 @@ class AlienInvasion():
             # Уничтожение существующих снарядов и создание новго флота
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
                     
     def _update_aliens(self):
         """Проверяет, достиг ли флот края экрана, 
@@ -105,6 +106,9 @@ class AlienInvasion():
         """Запускает новую игру при нажатии кнопки Play"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Сброс игровых настроек
+            self.settings.initialize_dynamic_settings()
+
             # Сброс игровой статистики
             self.stats.reset_stats()
             self.stats.game_active = True
